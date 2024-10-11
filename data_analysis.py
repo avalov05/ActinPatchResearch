c5b60378ddae8c2cae6d10ee152097115048ad3d
avalov05 <anton.valov05@gmail.com>
2024-10-11 10:44:36 -0400

    feat: csv to dict

--- data_analysis.py
+++ data_analysis.py
@@ -5,6 +5,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import csv
 
 
 #constants
@@ -64,7 +65,103 @@ def make_time_graphs(f_list, columns, destination):
                     print("Was not able to find the key...")
         plt.title("Time VS " + name)
         plt.savefig(destination + "/Time" + name + ".png")
-        plt.cla()
+
+def find_column_index_by_name(name, file):
+    with open(file) as csv_file:
+        csv_data = csv.reader(csv_file)
+        counter = 0
+        index = None
+        for line in csv_data:
+            for column in line:
+                if column == name:
+                    index = counter
+                counter += 1
+            if index == None:
+                print(name + " does not exist...")
+        return index
+
+def line_num_in_csv(file):
+    with open(file) as csv_f:
+        reader = csv.reader(csv_f)
+        return len(list(reader))
+
+def multiple_csv_to_fill_dict(f_list, columns):
+    data_dict={}
+    for name in columns:
+        if name != "Time" and name != "time":
+            data_dict[name] = []
+            column_index = find_column_index_by_name(name, f_list[0])
+            #print(line_num_in_csv(f_list[0]))
+            for line_num in range(1,line_num_in_csv(f_list[0])):
+                sum = 0
+                min = None
+                max = 0
+                for file in f_list:
+                    with open(file) as file:
+                        line_count = 0
+                        csv_f = csv.reader(file)
+                        for line in csv_f:
+                            if line_count == line_num:
+                                item = float(line[column_index])
+                                sum += item
+                                if max < item:
+                                    #print("max found")
+                                    max = item
+                                if min != None:
+                                    if min > item:
+                                        #print("min found")
+                                        min = item
+                                else:
+                                    min = item
+                            line_count += 1
+                    #print(name, line_num, min, max)
+                #print("------------")
+                average = sum/len(f_list)
+                #print(name, line_num, min, max, average)
+                data_dict[name].append({"min": min, "average": average, "max": max})
+    print(data_dict)
+    # data_dict={}
+    # for name in columns:
+    #     data_dict[name] = {
+            
+    #     }
+    #     if name != "Time" and name != "time":
+    #         for file in f_list:
+    #             file_items = []
+    #             with open(file) as csv_file:
+    #                 csv_data = csv.reader(csv_file)
+    #                 counter = 0
+    #                 index = None
+    #                 header = True
+    #                 for line in csv_data:
+    #                     for column in line:
+    #                         if column == name:
+    #                             index = counter
+    #                         counter += 1
+    #                     if index == None:
+    #                         print(name + " does not exist...")
+    #                         break
+    #                     if header != True:
+    #                         file_items.append(line[index])
+    #                     header = False
+    #             data_dict[name].append(file_items)
+    # print(data_dict)
+    
+
+        #         csv = pd.read_csv(file)
+        #         try:
+        #             #print(name)
+        #             #plt.scatter(csv["Time"], csv[name])
+        #             items.append(csv[name])
+        #         except KeyError:
+        #             print("Was not able to find the key...")
+        #     print(items)
+        # plt.title("Time VS " + name)
+        # plt.savefig(destination + "/Time" + name + ".png")
+        # plt.cla()
+
+def plot_column_fill():
+    print
 
 #functions-end
 
@@ -76,7 +173,7 @@ def main():
     csv_for_column_search_path = "/Users/antonvalov/Documents/PatchData/data/240611_ySM329_A_005 - Denoised_patch001.csv"
     #print(graph_combinations("/Users/antonvalov/Documents/PatchData/data/240611_ySM329_A_005 - Denoised_patch001.csv"))
     #read_csv(find_all_data("/Users/antonvalov/Documents/PatchData/data"), graph_combinations("/Users/antonvalov/Documents/PatchData/data/240611_ySM329_A_005 - Denoised_patch001.csv"))
-    make_time_graphs(find_all_data(csv_files_path), list_all_columns(csv_for_column_search_path), time_graph_folder_path)
+    multiple_csv_to_fill_dict(find_all_data(csv_files_path), list_all_columns(csv_for_column_search_path))
 
 #main call
 if __name__ == "__main__":
